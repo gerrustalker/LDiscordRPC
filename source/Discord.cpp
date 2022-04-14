cb1bbb69f638ff9644dfd54d8c3dbea574d7e0be
Andrey Stalker <shtieynikov@gmail.com>
2022-04-14 05:05:55 +0300

    Remove big commented piece of code

--- source/Discord.cpp
+++ source/Discord.cpp
@@ -2,28 +2,6 @@
 #include <iostream>;
 #include <chrono>;
 
-/*namespace Discord {
-    void Initialize() {
-        DiscordEventHandlers eventhandlers;
-        memset(&eventhandlers, 0, sizeof(eventhandlers));
-        Discord_Initialize("963257446049153074", &eventhandlers, 1, "4000");
-    }
-
-    void Update() {
-        DiscordRichPresence discordPresence;
-        memset(&discordPresence, 0, sizeof(discordPresence));
-        discordPresence.state = "In-game";
-        discordPresence.details = "Playing Garry's Mod";
-        discordPresence.startTimestamp = 1507665886;
-        //discordPresence.largeImageText = "Garry's Mod";
-        Discord_UpdatePresence(&discordPresence);
-    }
-
-    void Shutdown() {
-        Discord_Shutdown();
-    }
-*/
-
 void Discord::Initialize() {
     DiscordEventHandlers eventhandlers;
     memset(&eventhandlers, 0, sizeof(eventhandlers));
